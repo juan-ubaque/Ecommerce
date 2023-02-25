d35bf0839c78726a962d58e9e0adbbaf782e6a0c
Gaban98 <csrivera00@misena.edu.co>
2023-02-25 08:04:18 -0500

    respuesta https

--- product/views.py
+++ product/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def products(request):
+    return HttpResponse('Esta es la vista para los prodcutos')
