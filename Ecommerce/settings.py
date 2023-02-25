e91bfa3f1a9627ca2e57bd04c387c32f10d9d420
Gaban98 <csrivera00@misena.edu.co>
2023-02-25 07:25:06 -0500

    creacion de la app product

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'product', # primera app creada de productos
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'COT'
 
 USE_I18N = True
 
